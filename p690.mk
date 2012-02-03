a17ebe19e50ce15f7320c59c30dfb20cf06bd120
jst <asterione@gmail.com>
2012-02-03 13:40:06 +0100

    qualche modifica

--- p690.mk
+++ p690.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+$(call inherit-product, build/target/product/full_base.mk)
+
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
