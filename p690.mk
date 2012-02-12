b5b2e43b7f9fcd913f3549957af35bb2024616c8
jst <asterione@gmail.com>
2012-02-12 23:42:50 +0100

    some update

--- p690.mk
+++ p690.mk
@@ -12,7 +12,7 @@ PRODUCT_PACKAGES += \
     libmm-omxcore \
     libOmxCore \
     gps.p690 \
-    copybit.690 \
+    copybit.msm7k \
     LiveWallpapers \
     LiveWallpapersPicker \
     MagicSmokeWallpapers \
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lge/p690/proprietary/lib/hw/gralloc.msm7k.so:system/lib/hw/gralloc.msm7k.so \
     vendor/lge/p690/proprietary/lib/hw/gralloc.default.so:system/lib/hw/gralloc.default.so \
+    vendor/lge/p690/proprietary/lib/hw/lights.msm7k.so:system/lib/hw/lights.msm7k.so 
 
 ## Sensors
 PRODUCT_COPY_FILES += \
